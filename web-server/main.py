bf667409d6e67445c6e8c644c20cc3c7fc7bec16
Jersson Quintero <jersson.9.5@gmail.com>
2023-10-13 10:14:29 -0500

    Agregando modificaciones en web server

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3,4,5]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return '''
+        <h1>Hola soy una pagina web</h1>
+        <h2>Hola brayitan como estas?</h2>
+        <p> soy un parrafo </p>
+
+'''
 
 def run():
     store.get_categories()
